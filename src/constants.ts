f0d23f8f0c9307b570c2385d7223d41b7f9c5d83
Cameron Chrobocinski <cameronchrobo@gmail.com>
2025-06-27 17:23:50 -0500

    updated lhc version to 3

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const GITHUB_METADATA_HOST = "https://api.github.com";
 export const GITHUB_METADATA_REPOS_PATH = "/repos";
 export const GITHUB_USERNAME = "Camburgaler";
 export const REPO_NAME_LATIN_HYPERCUBE_GENERATOR = "latin-hypercube-generator";
-export const LHC_VERSION = "2";
+export const LHC_VERSION = "3";
